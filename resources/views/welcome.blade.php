a675e551104aab6052eb570d9b286b5816c5897e
FikriyahR065 <22696873-FikriyahR065@users.noreply.replit.com>
2023-05-03 18:14:14 +0000

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <link
+      href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900"
+      rel="stylesheet"
+    />
+
+    <title>Portofolio atin</title>
+    <!--template CSS-->
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="assets/css/templatemo-style.css" />
+  </head>
+
+  <body>
+    <div id="page-wraper">
+      <!-- Sidebar Menu -->
+      <div class="responsive-nav">
+        <div id="menu" class="menu">
+          <div class="container">
+            <div class="image">
+              <a href="#"><img src="assets/images/foto atin.JPG" alt="" /></a>
+            </div>
+            <div class="author-content">
+              <h4>Fikriyah Rahmatin</h4>
+              <span>Student</span>
+            </div>
+            <nav class="main-nav" role="navigation">
+              <ul class="main-menu">
+                <li><a href="#section1">About Me</a></li>
+                <li><a href="#section2">Riwayat Hidup</a></li>
+                <li><a href="#section3">Contact Me</a></li>
+              </ul>
+            </nav>
+            <div class="social-network">
+              <ul class="soial-icons">
+                <li>
+                  <a href="https://id-id.facebook.com/"
+                    ><i class="fa fa-facebook"></i
+                  ></a>
+                </li>
+                <li>
+                  <a href="https://twitter.com/?lang=id"><i class="fa fa-twitter"></i></a>
+                </li>
+                <li>
+                  <a href="https://id.linkedin.com/"><i class="fa fa-linkedin"></i></a>
+                </li>
+              </ul>
+            </div>
+            <div class="copyright-text">
+              <p>FR993</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <section class="section about-me" id="section1">
+        <div class="container">
+          <div class="section-heading">
+            <h2>About Me</h2>
+            <div class="line-dec"></div>
+            <span> A student of Infromation System class of 2021. A 19 year old girl living with a big hope. Loves to do and find out something that interseting. 
+            </span>
+          </div>
+          <div class="left-image-post">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="left-image">
+                  <img src="assets/images/bla bla.jpg" alt="" />
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="right-text">
+                  <h4>Related</h4>
+                  <p>I'm a student majoring infromation system, and i'm still beginner about web, so i'll learn about it to develop my skill.</p>
+                  <br>
+                  <ul>
+                    <li><strong>Nama:</strong> <span>Fikriyah Rahmatin</span>
+                    </li>
+                    <li><strong>Tempat/Tgl Lahir:</strong> <span>Bandar Lampung, 9 Septemberr 2003</span>
+                    </li>
+                    <li><strong>Email:</strong> <span>fikriyahrohmatin47@gmail.com</span></li>
+                    <li><strong>No Hp:</strong> <span>+62 85609955669</span></li>
+                  </ul>
+                  <ul>
+                    <li><strong>Umur:</strong> <span>19 Tahun</span></li>
+                    <li><strong>Status Pernikahan:</strong> <span>Belum Kawin</span>
+                    </li>
+                    <li> <strong>Pekerjaan:</strong> <span>Mahasiswa</span>
+                    </li>
+                    <li> <strong>Alamat:</strong> <span>Desa Suko Binangun, SB 11, Kec. Way Seputih, Kab. Lampung Tengah. </span></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="section my-services" id="section2">
+        <div class="container">
+          <div class="section-heading">
+            <h2>Riwayat Hidup</h2>
+            <div class="line-dec"></div>
+            <span> Here's my education</span>
+          </div>
+          <div class="row">
+            <div class="col-md-6">
+              <div class="service-item">
+                <h4>2009 - 2015</h4>
+                <p>SDS 1 Gula Putih Mataram</p>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="service-item">
+                <h4>2015 - 2018</h4>
+                <p>SMP Sugar Group Companies</p>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="service-item">
+                <h4>2018 -  2021</h4>
+                <p>SMA Negeri 1 Seputih Banyak <br>  Majoring in science.</p>
+              
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="service-item">
+                <h4>2021 - now</h4>
+                <p> Universitas Sriwijaya <br>Information System, Computer Science Faculty. </p>
+              
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="section contact-me" id="section3">
+        <div class="container">
+          <div class="section-heading">
+            <h2>Contact Me</h2>
+            <div class="line-dec"></div>
+            <span>If you have something to inform yo can contact me by clicking the icon of the social media or with this form</span>
+          </div>
+          
+          <div class="row">
+            <div class="right-content">
+              <div class="container">
+                <form id="contact" action="" method="post">
+                  <div class="row">
+                    <div class="col-md-6">
+                      <fieldset>
+                        <input
+                          name="name"
+                          type="text"
+                          class="form-control"
+                          id="name"
+                          placeholder="Your name..."
+                          required=""
+                        />
+                      </fieldset>
+                    </div>
+                    <div class="col-md-6">
+                      <fieldset>
+                        <input
+                          name="email"
+                          type="text"
+                          class="form-control"
+                          id="email"
+                          placeholder="Your email..."
+                          required=""
+                        />
+                      </fieldset>
+                    </div>
+                    <div class="col-md-12">
+                      <fieldset>
+                        <input
+                          name="subject"
+                          type="text"
+                          class="form-control"
+                          id="subject"
+                          placeholder="Subject..."
+                          required=""
+                        />
+                      </fieldset>
+                    </div>
+                    <div class="col-md-12">
+                      <fieldset>
+                        <textarea
+                          name="message"
+                          rows="6"
+                          class="form-control"
+                          id="message"
+                          placeholder="Your message..."
+                          required=""
+                        ></textarea>
+                      </fieldset>
+                    </div>
+                    <div class="col-md-12">
+                      <fieldset>
+                        <button type="submit" id="form-submit" class="button">
+                          Send Message
+                        </button>
+                      </fieldset>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+
+    <!-- Scripts -->
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <script>
+      
+    </script>
+  </body>
+</html>
